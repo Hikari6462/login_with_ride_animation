b997b849059ddb59fa2dbeb97ab1673220f37146
Hikari6462 <juanantoniocelvazquez@gmail.com>
2026-03-10 19:58:31 -0600

    feat(login): add success/fail triggers with email and password regex validation

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -25,6 +25,46 @@ class _LoginScreenState extends State<LoginScreen> {
   final _passwordFocusNode = FocusNode();
 
   Timer? _typingDebounce;
+  //controlers para manipular texto
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  ///errores que se veran en el ui
+  String? _emailError;
+  String? _passwordError;
+
+  //validadores
+  bool isValidEmail(String email) {
+    // Expresión regular simple para validar email
+    final emailRegex = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+    return emailRegex.hasMatch(email);
+  }
+
+  bool isValidPassword(String password) {
+    // Validar que la contraseña tenga al menos 8 caracteres una mayuscula, una minuscula, un digito y un especial
+    return RegExp(
+      r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$',
+    ).hasMatch(password);
+  }
+
+  void _onLogin() {
+    final email = _emailController.text.trim();
+    final password = _passwordController.text.trim();
+
+    setState(() {
+      // Validamos y asignamos errores si es necesario
+      _emailError = isValidEmail(email) ? null : 'Email inválido';
+      _passwordError = isValidPassword(password)
+          ? null
+          : 'La contraseña requiere Mayúscula, minúscula, # y especial';
+    });
+
+    if (_emailError == null && _passwordError == null) {
+      _isSuccess?.fire(); // Disparamos éxito
+    } else {
+      _trigFail?.fire(); // Disparamos fallo
+    }
+  }
 
   //2) Listeners para FocusNode (Oyentes/Chismosos)
 
@@ -91,6 +131,7 @@ class _LoginScreenState extends State<LoginScreen> {
               TextField(
                 //1.3 Asignar FocusNode al TextField
                 focusNode: _emailFocusNode,
+                controller: _emailController,
                 onChanged: (value) {
                   // Cuando el usuario escribe, el oso mira el campo
                   _isHandsUp?.value = false;
@@ -109,6 +150,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
                   hintText: 'Email',
+                  errorText: _emailError, // Mostramos el error si existe
                   prefixIcon: const Icon(Icons.email),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12),
@@ -119,6 +161,7 @@ class _LoginScreenState extends State<LoginScreen> {
               TextField(
                 //1.3 Asignar FocusNode al TextField
                 focusNode: _passwordFocusNode,
+                controller: _passwordController,
                 onChanged: (value) {
                   // Cuando el usuario escribe, el oso mira el campo
                   if (_isChecking != null) {
@@ -135,6 +178,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 obscureText: _obscureText,
                 decoration: InputDecoration(
                   hintText: 'Password',
+                  errorText: _passwordError, // Mostramos el error si existe
                   prefixIcon: const Icon(Icons.lock),
                   suffixIcon: IconButton(
                     icon: Icon(
@@ -144,8 +188,9 @@ class _LoginScreenState extends State<LoginScreen> {
                       setState(() {
                         _obscureText = !_obscureText;
                         // Si mostramos la clave, el oso baja las manos
-                        if (_isHandsUp != null)
+                        if (_isHandsUp != null) {
                           _isHandsUp!.value = _obscureText;
+                        }
                       });
                     },
                   ),
@@ -154,7 +199,53 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
               ),
+              const SizedBox(height: 10),
+              // Texto olvide mi contraseña
+              SizedBox(
+                width: size.width,
+                child: const Text(
+                  "Forgot password?",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
               const SizedBox(height: 20),
+              MaterialButton(
+                minWidth: size.width,
+                height: 50,
+                color: Colors.blueAccent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                onPressed: _onLogin, // Llamamos a la función de login corregida
+                child: const Text(
+                  "Login",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+
+              // No tienes cuenta
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account?"),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Register",
+                      style: TextStyle(
+                        color: Colors.blueAccent,
+                        decoration: TextDecoration.underline,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
@@ -165,6 +256,8 @@ class _LoginScreenState extends State<LoginScreen> {
   //1.4 Liberar memoria/recursos al salir de la pantallla
   @override
   void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
     _emailFocusNode.dispose();
     _passwordFocusNode.dispose();
     _typingDebounce?.cancel();
